3e87e197efbd0327e366f925e13da71fe035c6fd
kenigbolo <kenigbol@ut.ee>
2016-11-16 01:24:37 +0200

    Implement login to allow users sign-in with either thier username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,33 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validates :username, presence: true, uniqueness: {case_sensitive: false}
+  validate :validate_username
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+    	conditions[:email].downcase! if conditions[:email]
+			where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    end
+  end
+
+	def validate_username
+	  if User.where(email: username).exists?
+	    errors.add(:username, :invalid)
+	  end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: "home#index"
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout'}
+	root to: "home#index"
 end
