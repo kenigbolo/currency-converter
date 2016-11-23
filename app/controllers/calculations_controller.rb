2d7caf61781d4d52aeea33014b640b53af4a3386
kenigbolo <kenigbol@ut.ee>
2016-11-23 15:13:06 +0200

    Create action for calculation

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,5 +1,27 @@
 class CalculationsController < ApplicationController
   def create
-    render plain: "Viola!!!"
+    base = params[:calculation][:base_currency]
+    conversion = params[:calculation][:conversion_currency]
+    if base == conversion
+      flash[:notice] = "You cannot convert from #{base} to #{conversion}"
+    else
+      save_calculation(calculation_params)
+    end
+  end
+  private
+  def calculation_params
+    params.require(:calculation).permit(:base_currency, :conversion_currency, :amount, :num_of_days)
+  end
+  def save_calculation(calculation_params)
+    calculation = Calculation.new(calculation_params)
+    begin
+      ActiveRecord::Base.transaction do
+        calculation.save!
+      end
+      flash[:success_html_safe] = true
+    rescue => e
+      Rails.logger.warn e
+      flash['error'] = 'Calculation could not be processed'
+    end
   end
 end
