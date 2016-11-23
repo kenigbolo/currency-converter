f3c219ae7631e2ae345086e06f61484e5e44f579
kenigbolo <kenigbol@ut.ee>
2016-11-23 11:01:44 +0200

    Update routes for calculations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout'}
 	root to: "home#index"
-  resources :calculations
+  resources :calculations, only: [:create, :edit, :destroy]
 end
