c4568feb68e94bf60bb387e443c02b81c1ac6dd3
kenigbolo <kenigbol@ut.ee>
2016-11-23 10:51:26 +0200

    Generate calculation controller, helper and spec

---
+++ app/controllers/calculation_controller.rb
@@ -0,0 +1,2 @@
+class CalculationController < ApplicationController
+end
---
+++ spec/helpers/calculation_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the CalculationHelper. For example:
+#
+# describe CalculationHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe CalculationHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
