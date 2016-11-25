e7c1cf2e47d53c21fc6354b8e44783b02ab1714c
kenigbolo <kenigbol@ut.ee>
2016-11-25 22:09:37 +0200

    Remove asset handling statement in application.yml

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,5 @@ module CurrencyCoverter
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.configure do |env|
-      if Rails.env.development? || Rails.env.test?
-        env.cache = ActiveSupport::Cache.lookup_store(:memory_store)
-      end
-    end
   end
 end
