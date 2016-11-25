ec60033e3c8d12a05c061e8a459f7a1bb061508e
kenigbolo <kenigbol@ut.ee>
2016-11-25 23:56:54 +0200

    require main.js.erb in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require main
 //= require Chart.bundle
 //= require highcharts.js
 //= require chartkick
