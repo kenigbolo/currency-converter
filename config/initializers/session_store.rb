5e6f28415270d9a9b2d23feaeb949d85219a4708
kenigbolo <kenigbol@ut.ee>
2016-11-26 01:03:41 +0200

    Problem with CSRF token test resolve

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_currency-coverter_session_#{Rails.env}", domain: :all
+Rails.application.config.session_store :cookie_store, key: "_currency-coverter_session"
