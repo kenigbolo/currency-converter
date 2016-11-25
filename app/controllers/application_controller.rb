1e47b25211279d9eb723d23366a16d275e882d02
kenigbolo <kenigbol@ut.ee>
2016-11-26 00:33:51 +0200

    Attempt to fix errors for CSFR token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  rescue_from ActionController::InvalidAuthenticityToken do |exception|
+    sign_out current_user # Example method that will destroy the user cookies
+  end
   protected
 
   def configure_permitted_parameters
