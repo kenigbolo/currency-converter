da1ec19c07b35be1f47974829ca68ebccfe9d957
kenigbolo <kenigbol@ut.ee>
2016-11-27 05:17:21 +0200

    Implement github login logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,29 @@ class User < ApplicationRecord
 	    errors.add(:username, :invalid)
 	  end
 	end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.username = auth.info.nickname
+      user.save!
+    end
+  end
+
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def password_required?
+    super && provider.blank?
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,18 +7,24 @@ RSpec.describe User, type: :model do
       end
     end
 
+    context "User from omniauth" do
+      describe ".from_omniauth" do
+        it "is expected to be_valid" do
+          user = omniauth
+          expect(user).to be_valid
+        end
+      end
+
+      describe ".from_omniauth" do
+        it "saves new user" do
+          omniauth
+          expect(User.all).should_not be_nil
+        end
+      end
+    end
+
     before :each do
       @user = FactoryGirl.build(:user)
-    end
-
-    describe ".from_omniauth" do
-      it {is_expected.to be_valid}
-    end
-
-    describe ".from_omniauth" do
-      it "saves new user" do
-        expect(User.all).should_not be_nil
-      end
     end
 
     context "it validates the credentials properly" do
